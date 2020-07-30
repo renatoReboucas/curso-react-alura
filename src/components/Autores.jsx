23136a8dec8396e2e613d02fe8d8af40e5c40e7d
Renato Rebouças <renatoreboucas@MacBook-Pro-de-Renato.local>
2020-07-30 11:53:45 -0300

    implementando restante das paginas

--- src/components/Autores.jsx
+++ src/components/Autores.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import Header from './Header'
+import DataTable from './DataTable';
+
 export default function Autores(props) {
+
+  const [autores, setAutores] = useState([
+    {
+      nome: 'Paulo',
+      livro: 'React',
+      preco: '1000'
+    },
+    {
+      nome: 'Daniel',
+      livro: 'Java',
+      preco: '99'
+    },
+    {
+      nome: 'Renato',
+      livro: 'Design',
+      preco: '150'
+    },
+    {
+      nome: 'Bruno',
+      livro: 'DevOps',
+      preco: '100'
+    },
+  ]);
+  const titulo = 'Autores'
 
   return (
     <>
       <Header />
-      <h1>Pagina de Autores</h1>
+      <div className='container'>
+        <h1>Página de Autores</h1>
+        <DataTable dados={autores} titulo={titulo} colunas={['nome']} />
+      </div>
     </>
   )
 }
--- src/components/Livros.jsx
+++ src/components/Livros.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import Header from './Header'
+import DataTable from './DataTable';
+
 export default function Livros(props) {
+
+  const [autores, setAutores] = useState([
+    {
+      nome: 'Paulo',
+      livro: 'React',
+      preco: '1000'
+    },
+    {
+      nome: 'Daniel',
+      livro: 'Java',
+      preco: '99'
+    },
+    {
+      nome: 'Renato',
+      livro: 'Design',
+      preco: '150'
+    },
+    {
+      nome: 'Bruno',
+      livro: 'DevOps',
+      preco: '100'
+    },
+  ]);
+  const titulo = 'Livros'
 
   return (
     <>
       <Header />
-      <h1>Livros</h1>
+      <div className='container'>
+        <h1>Página de Livros</h1>
+        <DataTable dados={autores} titulo={titulo} colunas={['livro']} />
+      </div>
     </>
   )
 }
